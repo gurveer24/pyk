0af930e2b6dfcbd675f4e4d25785d556c02d7f1d
gurveer24 <54507661+gurveer24@users.noreply.github.com>
2020-01-12 17:13:13 +0530

    Update lesson_3.py

--- lesson_3.py
+++ lesson_3.py
@@ -1,2 +1,9 @@
-c=[1,2,3,4,5]
-print(c[-1,-2,-3,-4,-0])
+g=3,6,3,6
+print(g[0,-1,-2,-3])
+
+b=["gunnu","abhi","mamu","papa","mom"]
+c=["wheat","bajra"]
+print(b+c)
+
+gunnu=[[5,3],[8,3],[0,2],[9,1]]
+print(gunnu)
